048ec9e5a6768b6afed2da6745fb8c8b8f6fd80c
pdebelak <pdebelak@gmail.com>
2015-01-30 08:21:34 -0600

    Initial set up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mdtown::Application.config.secret_key_base = 'a8aca6d436a3c674693968654d208dced29eb76aa3624fd882d88d3a365ab2702ee6d120f1685e437965bc2ec969cc8682d74d0a33b478cbcacaa7971788c1af'
